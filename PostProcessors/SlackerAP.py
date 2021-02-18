884224e933f02f237f38d57e0f017e0964e3a782
Brian Hanson <brian@armourpeak.com>
2021-02-18 18:29:25 -0500

    added correct file this time

--- PostProcessors/SlackerAP.py
+++ PostProcessors/SlackerAP.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python
-
+#
+# Copyright 2021 Brian Hanson
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from autopkglib import Processor, ProcessorError
 
@@ -8,23 +21,72 @@ import os.path
 import json
 import requests
 
-slack_token = 'xoxb-my-bot-token'
-slack_channel = '#my-channel'
-slack_icon_url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuGqps7ZafuzUsViFGIremEL2a3NR0KO0s0RTCMXmzmREJd5m4MA&s'
-slack_user_name = 'Double Images Monitor'
+# Set the webhook_url to the one provided by Slack when you create the webhook at https://my.slack.com/services/new/incoming-webhook/
+
+__all__ = ["Slacker"]
+
+class Slacker(Processor):
+    description = ("Posts to Slack via webhook based on output of a MunkiImporter. "
+                    "Based on Graham Pugh's slacker.py - https://github.com/grahampugh/recipes/blob/master/PostProcessors/slacker.py"
+                    "and "
+                    "@thehill idea on macadmin slack - https://macadmins.slack.com/archives/CBF6D0B97/p1542379199001400"
+                    "Takes elements from " "https://gist.github.com/devStepsize/b1b795309a217d24566dcc0ad136f784"
+                    "and "
+                    "https://github.com/autopkg/nmcspadden-recipes/blob/master/PostProcessors/Yo.py")
+
+    input_variables = {
+        "munki_info": {
+            "required": False,
+            "description": ("Munki info dictionary to use to display info.")
+        },
+        "munki_repo_changed": {
+            "required": False,
+            "description": ("Whether or not item was imported.")
+        },
+        "webhook_url": {
+            "required": False,
+            "description": ("Slack webhook.")
+        }
+    }
+    output_variables = {
+    }
+
+    __doc__ = description
+
+    def main(self):
+        was_imported = self.env.get("munki_repo_changed")
+        munkiInfo = self.env.get("munki_info")
+        webhook_url = self.env.get("webhook_url")
+        slack_channel = self.env.get("slack_channel")
+
+        # Slack Custom Settings
+        ICONEMOJI = ":ghost:"
+        AUTOPKGICON = "https://avatars0.githubusercontent.com/u/5170557?s=200&v=4"
+        USERNAME = "AutoPKG"
+        slack_token = 'xoxb-230025915441-1775178689988-9AhJw7QJYhrTVeeiOO4WQuvW'
+        slack_channel = '#sps'
+        slack_icon_url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuGqps7ZafuzUsViFGIremEL2a3NR0KO0s0RTCMXmzmREJd5m4MA&s'
+        slack_user_name = 'SPS bot'
+
+        if was_imported:
+            name = self.env.get("munki_importer_summary_result")["data"]["name"]
+            version = self.env.get("munki_importer_summary_result")["data"]["version"]
+            pkg_path = self.env.get("munki_importer_summary_result")["data"]["pkg_repo_path"]
+            pkginfo_path = self.env.get("munki_importer_summary_result")["data"]["pkginfo_path"]
+            catalog = self.env.get("munki_importer_summary_result")["data"]["catalogs"]
+            if name:
+                slack_text = "*New item added to repo:*\nTitle: *%s*\nVersion: *%s*\nCatalog: *%s*\nPkg Path: *%s*\nPkginfo Path: *%s*" % (name, version, catalog, pkg_path, pkginfo_path)
+                slack_data = {'token': slack_token, 'text': slack_text, 'channel': slack_channel, 'icon_url': slack_icon_url, 'username': USERNAME}
+
+                response = requests.post(
+                'https://slack.com/api/chat.postMessage', json=slack_data)
+                if response.status_code != 200:
+                    raise ValueError(
+                                'Request to slack returned an error %s, the response is:\n%s'
+                                % (response.status_code, response.text)
+                                )
 
 
-def post_message_to_slack(text, blocks = None):
-    return requests.post('https://slack.com/api/chat.postMessage', {
-        'token': slack_token,
-        'channel': slack_channel,
-        'text': text,
-        'icon_url': slack_icon_url,
-        'username': slack_user_name,
-        'blocks': json.dumps(blocks) if blocks else None
-    }).json()	
-    
-    slack_info = 'There are *{}* double images detected for *{}* products. Please check the <https://{}.s3-eu-west-1.amazonaws.com/{}|Double Images Monitor>.'.format(double_images_count,
-        products_count, bucket_name, file_name)
-
-post_message_to_slack(slack_info)
+if __name__ == "__main__":
+    processor = Slacker()
+    processor.execute_shell()
